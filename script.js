5ed33131fb87291ce911fbbf506d7bf21f6d4399
Minard-NG <michael4sure20@gmail.com>
2021-12-18 19:44:40 +0100

    updated email validation bug

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ let btn = document.querySelector('button');
 let email = document.querySelector('input[type="email"]');
 let error = document.querySelector('svg.error');
 
+let re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
 
 btn.addEventListener('click', (e)=>{
-    if (email.value === '' || !email.validity.valid){
+    if (email.value === '' || !email.value.match(re)){
         error.style.opacity = 1;
         email.style.border = "1px solid #f96262";
     }
     else{
         email.value='';
+        error.style.opacity = 0;
         alert("You just signed up for greatness, keep an eye on your mail!")
     }
    
